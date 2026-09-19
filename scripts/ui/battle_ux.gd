e34fd7a3864c371e1ef81e6f21e515d9447deeab
ehb1127-arch <ehb1127@gmail.com>
2026-09-19 10:18:50 +0900

    Add touch-first combat selection feedback

--- scripts/ui/battle_ux.gd
+++ scripts/ui/battle_ux.gd
@@ -1,5 +1,5 @@
 InvalidOperation: 
 Line |
-   2 |  [IO.File]::ReadAllText('work/battle-ux/scenes/match.tscn',[Text.UTF8E ��
+   2 |  [IO.File]::ReadAllText('work/battle-ux/scripts/ui/battle_ux.gd',[Text ��
      |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      | Cannot create type. Only core types are supported in this language mode.
